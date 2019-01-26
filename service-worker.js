1fcd240e5bd77ac5f22e1b9da7a9f0b808374bac
Ravi Kumar Prasad <7ravikp@gmail.com>
2019-01-27 05:14:20 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c19a7f13475dbd7821f29b35cbcb4d27.js"
+  "/precache-manifest.27a91a6aef48991b8573c73a12c32b87.js"
 );
 
 workbox.clientsClaim();
