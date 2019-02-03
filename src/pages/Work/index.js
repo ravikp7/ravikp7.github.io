4c087c1397d967301f61a96a823a238cc30beb2e
Ravi Kumar Prasad <7ravikp@gmail.com>
2019-02-03 21:11:55 +0530

    feat: add work page

--- src/pages/Work/index.js
+++ src/pages/Work/index.js
@@ -3,7 +3,15 @@ import style from './style.module.css';
 
 const WorkPage = () => (
   <div className={style.main}>
-    <React.Fragment />
+    <div className={style.cardContainer}>
+      <div className={style.work}>
+        <React.Fragment />
+      </div>
+      <div className={style.work}>
+        <React.Fragment />
+      </div>
+    </div>
+    <div className={style.infoText}>Wanna see some code or resume ?</div>
   </div>
 );
 
