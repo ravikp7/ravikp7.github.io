454211d32db5fbfd5260705c20f6955509e96cee
Ravi Kumar Prasad <7ravikp@gmail.com>
2019-04-04 05:19:43 +0530

    fix: add mock for window.matchMedia in test

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+window.matchMedia = jest.fn().mockImplementation(query => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: jest.fn(),
+  removeListener: jest.fn(),
+}));
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
