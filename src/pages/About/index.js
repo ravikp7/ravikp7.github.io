cdbc4f96f41927422bedf21f41c0826a9620fe5a
Ravi Kumar Prasad <7ravikp@gmail.com>
2019-04-04 21:56:51 +0530

    chore: add test, lint in ci and fix lint

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -7,12 +7,13 @@ const About = () => (
     <div className={style.aboutContent}>
       I am making and breaking things since I was a kid. I spend most of my time coding or tweaking
       hardware and electronics stuff. I enjoy solving problems as they keep me alive. I care a lot
-      about efficient processes and maintainable software. I've a very simple goal in life to work
-      towards making this world a better place.
+      about efficient processes and maintainable software. I&apos;ve a very simple goal in life to
+      work towards making this world a better place.
     </div>
     <div className={style.quoteHeader}>A quote I strongly believe in</div>
     <div className={style.quoteContent}>
-      "It's not that I'm so smart, it's just that I stay with problems longer"- Albert Einstein
+    &quot;It&apos;s not that I&apos;m so smart,
+    it&apos;s just that I stay with problems longer&quot;- Albert Einstein
     </div>
   </div>
 );
